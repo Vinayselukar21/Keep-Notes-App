c2cb0c9ec859425cc6beae30622aba9900c2b316
Vinayselukar21 <vselukar2102@gmail.com>
2021-10-09 20:30:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+console.log("Welcome to notes app. This is app.js");
+showNotes();
+
+// If user adds a note, add it to the localStorage
+let addBtn = document.getElementById("addBtn");
+addBtn.addEventListener("click", function (e) {
+  let addTxt = document.getElementById("addTxt");
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.push(addTxt.value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  addTxt.value = "";
+  //   console.log(notesObj);
+  showNotes();
+});
+
+function showNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  let string = "";
+  notesObj.forEach(function (element, index) {
+    string += `
+              <div class="noteCard my-2 mx-2" style="width: 18rem;">
+              <div class="card-body">
+                <h5 class="card-title">Note ${index + 1}</h5>
+                <p class="card-text">${element}</p>
+                <button id=${index} onclick=notesDelete(this.id) class="btn btn-primary">Delete Note</button>
+              </div> </div>`;
+  });
+  let notesElm = document.getElementById("notes");
+  if (notesObj.length != 0) {
+    notesElm.innerHTML = string;
+  } else {
+    notesElm.innerHTML = "Your notes will show here";
+  }
+}
+
+function notesDelete(index) {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+  console.log("You just deleted", index);
+  showNotes();
+}
+
+let search = document.getElementById("searchTxt");
+search.addEventListener("input", function () {
+  let inputVal = search.value.toLowerCase();
+  let cards = document.getElementsByClassName("noteCard");
+
+  Array.from(cards).forEach(function (element) {
+    let cardTxt = element.getElementsByTagName("p")[0].innerHTML;
+
+    if (cardTxt.includes(inputVal)) {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  });
+});
